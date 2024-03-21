09e4c041b6149987060c8bebb2022cb040441e14
ibrahimesseddyq <https.ibrahim.esseddyq@gmail.com>
2024-03-21 15:53:47 +0000

    [iesseddyq][automata , eeh yaliyam]

--- src/vm.h
+++ src/vm.h
@@ -1,15 +1,24 @@
 #ifndef VM_H
 #define VM_H
 #include <stack>
+typedef stack<uint8> stack8
     /*
         mstack represents the main stack 
         //TODO 
-        -add registers
-        -add a processor stimulator
+        -get some deep knowledge on Automata Theory :)
     */
+    class x_cpu
+    {
+        stack8 r1;
+        stack8 r2;
+        stack8 r3;
+
+        uint32 PC;
+    }
     class vm
     {
         public:
-            stack<uint8> mstack;
+            x_cpu  cpu;  
+            stack8 mstack;
     }
 #endif
