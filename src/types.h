19da1205b72b72c736083fcb93648323e111532b
ibrahimesseddyq <https.ibrahim.esseddyq@gmail.com>
2024-03-21 15:39:34 +0000

    [iesseddyq][vm class]

---
+++ src/types.h
@@ -0,0 +1,14 @@
+#ifndef TYPES_H
+#define TYPES_H
+#define uint8 unsigned char 
+#define uint16 unsigned short
+#define uint32 unsigned int
+#define uint64 unsigned long long
+
+#define int8 char
+#define int16 short
+#define int32 int
+
+#define true 1
+#define false 0
+#endif
--- src/vm.h
+++ src/vm.h
@@ -1,4 +1,15 @@
 #ifndef VM_H
 #define VM_H
-
+#include <stack>
+    /*
+        mstack represents the main stack 
+        //TODO 
+        -add registers
+        -add a processor stimulator
+    */
+    class vm
+    {
+        public:
+            stack<uint8> mstack;
+    }
 #endif
